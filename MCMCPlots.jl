f7cf6bcb6e2a9310c96d92383f15385cbd36fe9a
Luca Geminiani <145434726+LucaGeminiani00@users.noreply.github.com>
2024-01-06 19:25:55 +0100

    Update MCMCPlots.jl

--- MCMCPlots.jl
+++ MCMCPlots.jl
@@ -11,10 +11,16 @@ function Runplots(Trace::Array{Int64, 3}, D::Int64, T::Int64, iter::Int64)
     for i = 1:T
         series[:,i] = Trace[d,i,:]
     end
-    l = @layout [a{0.01h}; grid(2,2)]
-    p = fill(plot(),5,1)
+    r = 0
+    if T % 2 == 0
+        r = div(T,2)
+    else 
+        r = convert(Int, ceil(T/2))
+    end 
+    l = @layout [a{0.01h}; grid(r,2)]
+    p = fill(plot(),T+1,1)
     p[1] = plot(title=tit,framestyle=nothing,showaxis=false,xticks=false,yticks=false,margin=0mm)
-    [p[i+1] = plot(time, series[:,i], linewidth=2, legend=false, title="topic:"*string(i),titlefont=13) for i in 1:4]
+    [p[i+1] = plot(time, series[:,i], linewidth=2, legend=false, titlefont=13) for i in 1:T]
     plot(p..., layout=l)
 end 
 end 
